5a72198dbfa79f618f76bfe128724270abaf4d3e
Redean <redean.akunzada@gmail.com>
2024-09-21 01:52:39 +0200

    v0.1.1

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import React from "react";
-import Particles from "./components/particles";
+import Particles from "../components/particles";
+import { Navigation } from "../components/nav";
 
 const navigation = [
   { name: "Projects", href: "/projects" },
@@ -10,36 +11,20 @@ const navigation = [
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-gradient-to-tl from-black via-zinc-600/20 to-black">
-      <nav className="my-16 animate-fade-in">
-        <ul className="flex items-center justify-center gap-4">
-          {navigation.map((item) => (
-            <Link
-              key={item.href}
-              href={item.href}
-              className="text-sm duration-500 text-zinc-500 hover:text-zinc-300"
-            >
-              {item.name}
-            </Link>
-          ))}
-        </ul>
-      </nav>
+      <Navigation />
       <div className="hidden w-screen h-px animate-glow md:hidden animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <Particles
         className="absolute inset-0 -z-10 animate-fade-in"
         quantity={100}
       />
-      <h1
-        className="py-3.5 px-0.5 z-10 text-4xl text-white bg-black cursor-default animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text font-bold tracking-tight drop-shadow-md transition-all duration-1000"
-      >
-        rdnzda
-      </h1>
 
       <div className="hidden w-screen h-px animate-glow md:hidden animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <div className="my-16 text-center animate-fade-in">
         <h2 className="text-sm text-zinc-500 ">
-          Hi my name is Redean Akunzada, i'm a fullstack web developper.
+          Still working on projects...
         </h2>
       </div>
     </div>
   );
+
 }
