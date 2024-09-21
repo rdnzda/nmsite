3e6a5ae2337a6d07b54af2cc8cb777056d057631
Redean <redean.akunzada@gmail.com>
2024-09-21 15:53:16 +0200

    v0.1.4 bug ios

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,10 +28,10 @@ export default function RootLayout({
   return (
     <html lang="en" className={[inter.variable, calSans.variable].join(" ")}>
       <body className={`bg-black ${process.env.NODE_ENV === "development" ? "debug-screens" : ""}`}>
-        <main className="flex flex-col items-center justify-center min-w-full min-h-full overflow-hidden bg-gradient-to-tl from-black via-zinc-600/20 to-black relative">
-          <div className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-gradient-to-tl from-black via-zinc-600/20 to-black">
-          <div className="hidden w-screen h-px animate-glow md:hidden animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
-          <div className="hidden w-screen h-px animate-glow md:hidden animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
+        <main className="main-container flex flex-col items-center justify-center min-w-full overflow-hidden bg-gradient-to-tl from-black via-zinc-600/20 to-black relative">
+          <div className="flex flex-col items-center justify-center w-screen min-h-screen overflow-hidden bg-gradient-to-tl from-black via-zinc-600/20 to-black">
+            <div className="hidden w-screen h-px animate-glow md:hidden animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
+            <div className="hidden w-screen h-px animate-glow md:hidden animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
             <Particles className="absolute inset-0 -z-10 animate-fade-in" quantity={100} />
             
             {/* Afficher la navigation seulement si ce n'est pas la page principale */}
