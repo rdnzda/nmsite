47c665979d89730a0acf7ed2229a51d267c24dfc
Redean <redean.akunzada@gmail.com>
2024-09-21 15:56:29 +0200

    v0.1.5 mb-6 mobile devices

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -28,7 +28,7 @@ export default function Example() {
 	return (
 		<div>
 			<div className="container flex items-center justify-center min-h-screen px-4 mx-auto">
-				<div className="grid w-full grid-cols-1 gap-8 mx-auto mt-32 sm:mt-0 sm:grid-cols-3 lg:gap-16">
+				<div className="grid w-full grid-cols-1 gap-8 mx-auto mt-32 sm:mt-0 sm:grid-cols-3 lg:gap-16 mb-6">
 					{socials.map((s, index) => (
 						<Card key={index}>
 							<Link
