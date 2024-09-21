47289dbdf9252d56ec9996d6f5c0bb9ca6d9b01c
Redean <redean.akunzada@gmail.com>
2024-09-21 02:43:00 +0200

    v0.1.2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,9 +9,9 @@ const navigation = [
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-gradient-to-tl from-black via-zinc-600/20 to-black">
-      <nav className="my-16 animate-fade-in">
-        <ul className="flex items-center justify-center gap-4">
+    <div className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-gradient-to-tl from-black via-zinc-600/20 to-black relative"> {/* Ajout de 'relative' ici */}
+      <nav className="absolute bottom-0 mb-4 animate-fade-in w-full"> {/* Modifications ici */}
+        <ul className="flex items-center justify-center md:gap-4 gap-5">
           {navigation.map((item) => (
             <Link
               key={item.href}
@@ -29,15 +29,15 @@ export default function Home() {
         quantity={100}
       />
       <h1
-        className="py-3.5 px-0.5 z-10 text-4xl text-white bg-black cursor-default animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text font-bold tracking-tight drop-shadow-md transition-all duration-1000"
+        className="py-3.5 px-0.5 z-10 text-7xl text-white bg-black cursor-default animate-title font-display sm:text-8xl md:text-8xl whitespace-nowrap bg-clip-text font-bold tracking-tight drop-shadow-md transition-all duration-1000"
       >
         rdnzda
       </h1>
 
       <div className="hidden w-screen h-px animate-glow md:hidden animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
-      <div className="my-16 text-center animate-fade-in">
-        <h2 className="text-sm text-zinc-500 ">
-          Hi my name is Redean Akunzada, i am a fullstack web developper.
+      <div className="sm:my-4 my-2 text-center animate-fade-in">
+        <h2 className="text-xs sm:text-sm text-zinc-500 ">
+          Hi my name is Redean Akunzada, I am a fullstack web developer.
         </h2>
       </div>
     </div>
